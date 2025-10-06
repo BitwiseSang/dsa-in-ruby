b1e61f56e95600f4732920f480ea0bd46e7f050a
BitwiseSang <135348362+BitwiseSang@users.noreply.github.com>
2025-10-06 10:16:15 +0300

    refactor: move hashmap_and_hashset dir to seperate dirs

---
+++ hashmap/main.rb
@@ -0,0 +1,46 @@
+require_relative 'lib/hashmap'
+
+test = HashMap.new
+puts test.length
+
+test.set('apple', 'red')
+test.set('banana', 'yellow')
+test.set('carrot', 'orange')
+test.set('dog', 'brown')
+test.set('elephant', 'gray')
+test.set('frog', 'green')
+test.set('grape', 'purple')
+test.set('hat', 'black')
+test.set('ice cream', 'white')
+test.set('jacket', 'blue')
+test.set('kite', 'pink')
+test.set('lion', 'golden')
+
+# Load factor at capacity
+puts test.length
+puts test.capacity
+print test.entries
+
+# Exceeding load factor capacity
+test.set('moon', 'silver')
+puts
+puts test.length
+puts test.capacity
+
+# Linked list implementation
+test.set('apple', 'red')
+test.set('banana', 'yellow')
+test.set('carrot', 'orange')
+test.set('dog', 'brown')
+test.set('elephant', 'gray')
+test.set('frog', 'green')
+test.set('grape', 'purple')
+test.set('hat', 'black')
+test.set('ice cream', 'white')
+test.set('jacket', 'blue')
+test.set('kite', 'pink')
+test.set('lion', 'golden')
+
+puts "Length after duplication: #{test.length}"
+puts "Capacity after duplication: #{test.capacity}"
+puts "Entries: #{test.entries}"
